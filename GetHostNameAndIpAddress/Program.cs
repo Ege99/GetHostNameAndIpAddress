da54525bd57f117e3cdf539c62de8317cc9da989
egesubat <egesub34@gmail.com>
2021-11-01 02:31:15 +0300

    Proje dosyası ekle.

---
+++ GetHostNameAndIpAddress/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace GetHostNameAndIpAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string HostName = Dns.GetHostName();
+            Console.WriteLine("Cihazin Host Adi : " + HostName);
+            IPAddress[] ipadress = Dns.GetHostAddresses(HostName);
+            Console.WriteLine("Cihazin IP Adresi : " +
+                "   Ilk Satir IPV6,   Ikinci Satir IPV4 icindir.");
+            foreach (IPAddress ip in ipadress)
+            {
+                Console.WriteLine(ip.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
